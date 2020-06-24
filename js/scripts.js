ca6a50a21b24352f9f56110c6cc393a98550f75d
Adrian Dancau <adi.dancau@gmail.com>
2020-06-24 20:28:11 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-let searchBox = document.querySelector('#search');
-let pictures = document.querySelectorAll('a');
+const searchBox = document.querySelector('#search');
+const pictures = document.querySelectorAll('a');
 
 searchBox.addEventListener('keyup', (event) => {
-    console.log(searchBox.value);
     for( let i = 0; i < pictures.length; i++ ) {
-        if( searchBox.value.length == 0 ){
-            pictures[i].style.display = 'inline';
-        }
-        else if(pictures[i].dataset.title.includes(searchBox.value)) {
+        if(pictures[i].dataset.title.includes(searchBox.value.toLowerCase()) || searchBox.value.length == 0) {
             pictures[i].style.display = 'inline';
         } else {
             pictures[i].style.display = 'none';
